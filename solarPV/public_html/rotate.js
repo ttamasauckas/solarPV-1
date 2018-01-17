7b1745e0a74ecd712ee4fe9e2f633acfdde11b44
ttamasauckas <tyler.j.tamasauckas@gmail.com>
2018-01-17 10:54:35 -0700

    MoreFeatures
    Added jquery slideshow, html5 video, changed form layout a bit.

---
+++ solarPV/public_html/rotate.js
@@ -0,0 +1,20 @@
+function rotateBanner(banner) {
+    var active = $(banner+" img.active");
+    var next = active.next();
+    if (next.length == 0)
+            next = $(banner+" img:first");
+    active.removeClass("active").fadeOut(400);
+    next.addClass("active").fadeIn(400);
+
+}
+
+function prepare(banner) {
+    $(banner+" img").fadeOut(0);
+    $(banner+" img:first").fadeIn(0).addClass("active");
+
+}
+
+function start(banner) {
+    prepare(banner);
+    setInterval("rotateBanner('"+banner+"')", 6000);
+}
